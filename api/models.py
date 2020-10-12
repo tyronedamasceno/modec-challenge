3929f70914e5f60107cff964b71f81d982476052
Tyrone Damasceno <tyronedamasceno@gmail.com>
2020-10-12 06:17:58 -0300

    set vessel code as primary_key

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Vessel(models.Model):
-    code = models.CharField('Vessel Code', max_length=20, unique=True)
+    code = models.CharField('Vessel code', max_length=20, primary_key=True)
 
     def __str__(self):
         return f'Vessel {self.code}'
