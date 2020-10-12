a89dbdb97f0f7458e345394f481c559e9d9cac96
Tyrone Damasceno <tyronedamasceno@gmail.com>
2020-10-12 09:09:16 -0300

    change some settings

--- modec/settings.py
+++ modec/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kxe9yka8a__8pz217)wc)rp!%4cncs^2+r!hr(nld1_a+)nawm'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'kxe9yka8a__8pz217)wc)rp!%4cncs^2+r!hr(nld1_a+)nawm')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", ".herokuapp.com"]
 
 
 # Application definition
