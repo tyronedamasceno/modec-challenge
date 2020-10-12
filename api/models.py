d3b97a6da53ab2ecebeeb1ad5d6082d75bbdfcdf
Tyrone Damasceno <tyronedamasceno@gmail.com>
2020-10-12 06:27:59 -0300

    equipment model

--- api/models.py
+++ api/models.py
@@ -1,8 +1,25 @@
 from django.db import models
 
+from api.enums import EquipmentStatus
+
 
 class Vessel(models.Model):
     code = models.CharField('Vessel code', max_length=20, primary_key=True)
 
     def __str__(self):
         return f'Vessel {self.code}'
+
+
+class Equipment(models.Model):
+    code = models.CharField('Equipment code', max_length=20, primary_key=True)
+    name = models.CharField('Equipment name', max_length=50)
+    location = models.CharField('Equipment location', max_length=50)
+    status = models.CharField(
+        'Equipment status', max_length=20, choices=EquipmentStatus.choices(),
+        default=EquipmentStatus.active
+    )
+
+    vessel = models.ForeignKey(Vessel, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Equipment {self.code} - {self.status.name}'
